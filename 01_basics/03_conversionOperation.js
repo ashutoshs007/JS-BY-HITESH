d9901192473292774e66aebc0c1f2e35c5dfe2e8
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-06-22 23:52:35 +0530

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -131,12 +131,64 @@ and and true gives 1 and false gives 0 */
 //""->empty string gives false
 //" three" ->value in string gives true
 
-let someNumber =33
+// let someNumber =33
 
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber))
-console.log("harsh")
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+
+
+//***************** OPERATIONS  ****************
+
+// let value = 3 
+// let negValue= -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1="hello"
+// let str2=" ashutosh"
+// let str3=str1+str2
+// console.log(str3);
+
+
+
+// console.log(2+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");//jo type phle hoga
+// //  waise hi treat hoga final ans
+// console.log(3 + 4 * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 =num3= 2 + 2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
 
 
 
