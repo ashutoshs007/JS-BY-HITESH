245ed5ab73bc42f7c4e72bd3c08ee39644e9d9f3
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-06-24 18:31:38 +0530

    datatypesummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+//Primitive
+
+// types: String,Number,Boolean,null,undefined,Symbol,
+// BigInt
+
+const score=100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+// const id =Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id ===anotherId);
+
+
+// const bigNumber = 2384229428952n
+
+//Array,objects ,functions
+
+const heros =["shaktimaan","naagraj","doga"];
+// let myObj={
+//     name:"ashutosh",
+//     age:22,
+
+// }
+
+const myFunction =function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myFunction);
+// /*object function returns function*/(function object)
+
+
+console.log(typeof bigNumber); 
+console.log(typeof outsideTemp); 
+console.log(typeof scoreValue);
+console.log(typeof heros);
