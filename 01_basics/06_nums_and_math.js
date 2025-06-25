e5088aa0f0131e9af9debcdaf11131c9f709777a
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-06-25 17:34:40 +0530

    changes

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,4 +1,75 @@
-const score = 400
+// const score = 400
+// console.log(score);
 
-const balance = new Number(100)
-console.log(balance);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); 
+// // use too fised always whenever making an e-commerce 
+// // application ,coz customer needs precision values
+
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const anotherNumber = 1234.343
+// console.log(anotherNumber.toPrecision(3));
+
+
+// const hundreds =1000000
+
+// console.log(hundreds.toLocaleString());//US STANDARD
+// console.log(hundreds.toLocaleString('en-IN'));//INDIAN STANDARD
+
+
+
+//++++++++++++++++++++++=Maths+++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// // changes to positive value
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// //ceil mtlb ceiling choose krunga(top value)
+// // agr zra sa bhi 4 se bda hua to round off krne ke liye
+
+
+// console.log(Math.floor(4.8)); 
+// //niche wali values lega  
+// console.log(Math.min(3,4,5,6));
+// console.log(Math.max(3,4,5,6));
+
+
+
+
+console.log(Math.random());  /*gives random values b/w 0 and 1*/
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);//*to get nummeric value*/
+
+
+const min=10
+const max=20
+console.log(Math.random() * (max-min+1))
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
