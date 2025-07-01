e9413ae9cf718ff5a59d3df9793494f40c26e1ad
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-07-01 16:16:48 +0530

    changes

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -53,9 +53,10 @@ console.log(Math.floor(Math.random()*10)+1);//*to get nummeric value*/
 
 const min=10
 const max=20
-console.log(Math.random() * (max-min+1))
+console.log(Math.floor(Math.random() * (max-min+1))+min)
 
 
+  
 
  
 
---
+++ 01_basics/08_datesinJS.js
@@ -0,0 +1,7 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate);
+console.log(myDate.toString());
+
+
