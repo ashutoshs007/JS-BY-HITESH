fed3ac48736b0a2aa04ed82153edfaa9f88b1be3
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-07-02 14:35:09 +0530

    json ta;k

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,108 +1,261 @@
-/* Object sigelton or how we can decalre object with 
-the help of constructor */
+// /* Object sigelton or how we can decalre object with 
+// the help of constructor */
 
-// const tinderUser = new Object() //(THIS IS SINGELTON OBJECT)
-const tinderUser = {}  // THIS IS NON-SINGELTON OBJECT
+// // const tinderUser = new Object() //(THIS IS SINGELTON OBJECT)
+// const tinderUser = {}  // THIS IS NON-SINGELTON OBJECT
 
-// console.log(tinderUser);
-//BOTH GIVES SAME output
+// // console.log(tinderUser);
+// //BOTH GIVES SAME output
 
-tinderUser.id = "123abc"
-tinderUser.name ="Sammy"
-tinderUser.isLoggedIn=false
+// tinderUser.id = "123abc"
+// tinderUser.name ="Sammy"
+// tinderUser.isLoggedIn=false
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-//Objects within objects
-const regularUser={
-    email: "some@gmail.com",
-    fullname: { 
-        userfullname:{
-            firstname:"hitesh",
-            lastname:"choudhary"
-        }
-    }
-}
+// //Objects within objects
+// const regularUser={
+//     email: "some@gmail.com",
+//     fullname: { 
+//         userfullname:{
+//             firstname:"hitesh",
+//             lastname:"choudhary"
+//         }
+//     }
+// }
 
-// console.log(regularUser.fullname);
-// console.log(regularUser.fullname.userfullname);
+// // console.log(regularUser.fullname);
+// // console.log(regularUser.fullname.userfullname);
 
-const obj1={1:"a", 2:"b"}
-const obj2={3:"a", 4:"b"}
-const obj4={5:"a", 6:"b"}
+// const obj1={1:"a", 2:"b"}
+// const obj2={3:"a", 4:"b"}
+// const obj4={5:"a", 6:"b"}
 
-// const obj3 ={obj1,obj2}
-/*one way to merge objects*/
+// // const obj3 ={obj1,obj2}
+// /*one way to merge objects*/
 
-// const obj3=Object.assign({},obj1,obj2,obj4)
-/*another way to merge objects*/
-// console.log(obj3);
+// // const obj3=Object.assign({},obj1,obj2,obj4)
+// /*another way to merge objects*/
+// // console.log(obj3);
 
-//{} is the target obj where all later(source) objects will get merged
-//for more infor check object merge mdn documentation in js 
+// //{} is the target obj where all later(source) objects will get merged
+// //for more infor check object merge mdn documentation in js 
 
-const obj3={...obj1,...obj2}
-/*third way to merge objects is using spread operator*/
-// console.log(obj3);
+// const obj3={...obj1,...obj2}
+// /*third way to merge objects is using spread operator*/
+// // console.log(obj3);
 
 
 
-/*jb database se value aayegi then use this syntax*/
-// (array of objects)
-const users= [
-    {
-        id:1,
-        email: "h@gmail.com"
-    },
-     {
-        id:2,
-        email: "ha@gmail.com"
-    }
+// /*jb database se value aayegi then use this syntax*/
+// // (array of objects)
+// const users= [
+//     {
+//         id:1,
+//         email: "h@gmail.com"
+//     },
+//      {
+//         id:2,
+//         email: "ha@gmail.com"
+//     }
      
     
-]
+// ]
 
-users[1].email
+// users[1].email
+// console.log(tinderUser);
+
+// // console.log(Object.keys(tinderUser));
+// /*this will give keys in array datatype*/
+// // now run loop in these or use wherever you want ,too important for upcoming projects or work in database
+
+
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+
+
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+
+
+
+// /*Object destructuring and JSON API*/
+
+
+// const course ={
+// coursename: "js in hindi",
+// price:"999",
+// courseInstructor:"hitesh"
+// }
+
+// // course.courseInstructor
+
+// const{courseInstructor}=course
+// console.log(courseInstructor);
+
+
+
+
+// const{courseInstructor:teacher}=course
+// console.log(teacher);//DESTRUCTURING THE OBJECT
+
+// /*
+// //Some methods in react
+// // const navbar = (props.company)=> {
+// const navbar = ({company})=> { //destructuring
+
+
+// } 
+// navbar(company = "hitesh")
+// */
+
+
+
+// /*API*/
+
+// //sometimes we get api in object format
+
+// /* keys and values both are string in json 
+//  boolean values and no do not come in ""*/
+//  {
+//     "name":"hitesh",
+//    "coursename": "js in hindi",
+//     "price": "free"
+
+//  }
+
+
+
+
+//  //sometimes we get api in array format
+
+
+//  /*objects in array */
+
+//  //json format
+//  [
+//     {},
+//     {},
+    
+//  ]
+
+
+
+
+// 🔸 Object Singleton vs Non-Singleton
+
+// const tinderUser = new Object(); // Singleton object (rarely used directly)
+const tinderUser = {}; // Non-Singleton object (commonly used)
+
+// Adding properties to the object
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
 console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-/*this will give keys in array datatype*/
-// now run loop in these or use wherever you want ,too important for upcoming projects or work in database
+// 🔸 Nested Objects
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "hitesh",
+      lastname: "choudhary"
+    }
+  }
+};
 
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
 
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// 🔸 Merging Objects
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
 
+// 1. Using Object.assign()
+const merged1 = Object.assign({}, obj1, obj2, obj4);
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// 2. Using spread operator
+const merged2 = { ...obj1, ...obj2, ...obj4 };
 
+console.log(merged1);
+console.log(merged2);
 
+// 🔸 Array of Objects (from DB/API)
+const users = [
+  {
+    id: 1,
+    email: "h@gmail.com"
+  },
+  {
+    id: 2,
+    email: "ha@gmail.com"
+  }
+];
 
+console.log(users[1].email);
 
+// 🔸 Object Utility Methods
+console.log(Object.keys(tinderUser));     // ➝ ['id', 'name', 'isLoggedIn']
+console.log(Object.values(tinderUser));   // ➝ ['123abc', 'Sammy', false]
+console.log(Object.entries(tinderUser));  // ➝ [['id','123abc'], ['name','Sammy'], ['isLoggedIn',false]]
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // ➝ true
 
+// 🔸 Object Destructuring
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "hitesh"
+};
 
+const { courseInstructor } = course;
+console.log(courseInstructor); // ➝ hitesh
 
+const { courseInstructor: teacher } = course;
+console.log(teacher); // ➝ hitesh
 
+// 🔸 Destructuring in React-style function
+const navbar = ({ company }) => {
+  console.log(`Company: ${company}`);
+};
 
-/*Object destructuring and JSON API*/
+navbar({ company: "hitesh" }); // Correct way to call it
 
+// 🔸 JSON Format Examples
 
-const course ={
-coursename: "js in hindi",
-price:"999",
-courseInstructor:"hitesh"
+// ✅ JSON Object format (commonly received from APIs)
+/*
+{
+  "name": "hitesh",
+  "coursename": "js in hindi",
+  "price": "free"
 }
+*/
 
-// course.courseInstructor
+// ✅ JSON Array of Objects format
+/*
+[
+  {
+    "name": "user1",
+    "email": "user1@example.com"
+  },
+  {
+    "name": "user2",
+    "email": "user2@example.com"
+  }
+]
+*/
 
-const{courseInstructor}=course
-console.log(courseInstructor);
 
 
 
 
-const{courseInstructor:teacher}=course
-console.log(teacher);
 
 
 
---
+++ 02_basics/wowdelete.js
@@ -0,0 +1,13 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   console.log("Request received:");
+//   console.log(req.method, req.url);
+  
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello, world!\n");
+// });
+
+// server.listen(8080, () => {
+//   console.log("Server started at port 8080");
+// }0;
