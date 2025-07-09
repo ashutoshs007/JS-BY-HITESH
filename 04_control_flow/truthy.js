2a50d34d39d9af3d8d678c3616c2b551e95a80df
ashutoshs007 <Ashutosh.12222528@lpu.in>
2025-07-09 11:20:51 +0530

    changes

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,33 @@
+const user ="h@hitesh.ai"
+// const user =""
+// const user =[]
+
+if(userEmail){
+    console.log("Got user email");
+}
+
+else{
+    console.log("do not have user email");
+    
+}
+
+
+//falsy values
+
+false,0,-0,BigInt(0n),"",null,undefined,NaN
+
+// rest all are truthy values
+eg; [],"0" ,'false'," ",{},function(){}//(empty function)
+
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+    
+
+// }
+
+
+// const emptyObj ={}
+
+// if(Obj)
+
+
